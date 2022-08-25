fac178f9e37b6169265f9e672af85ed5185c3626
Chris-Lazarius <Lazariuscm@gmail.com>
2022-08-25 14:53:10 +0200

    Additional feature 1

---
+++ src/ClientSide/MultiClient.java
@@ -0,0 +1,20 @@
+package ClientSide;
+
+import java.net.Socket;
+import java.sql.Date;
+
+public class MultiClient {
+    
+
+    /**
+     * @param args
+     */
+    public static void public static void main(String[] args) throws Exception {
+        Socket socket = new Socket("127.0.0.1", 8888);
+        
+
+
+    }
+
+
+}
