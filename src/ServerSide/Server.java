9cd8ab491e831a02d8da73bd36a234f9e2cb3383
Chris-Lazarius <Lazariuscm@gmail.com>
2022-08-25 21:39:20 +0200

    Server side commitment 1

---
+++ src/ServerSide/Server.java
@@ -0,0 +1,26 @@
+package ServerSide;
+
+import java.io.*;
+import java.net.*;
+
+
+public class Server {
+    /**
+     * @param args
+     * @throws IOException
+     */
+    public static void main(String[] args) throws IOException {
+    
+        try {
+            ServerSocket ssc = new ServerSocket(8888);
+            Socket sc = ssc.accept();
+            DataInputStream din = new DataInputStream(sc.getInputStream());
+            String sMsg = (String)din.readUTF();
+            System.out.println("message: " + sMsg);
+            ssc.close();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
